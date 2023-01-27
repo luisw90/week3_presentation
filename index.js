92635f9ab3de05ef07e0b8b0c5a96bca588726f5
Britta <brittan.villegas@gmail.com>
2023-01-27 10:31:36 +0100

    added modal functionality and styled slideshow arrows

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ let slideIndex = 1;
 const slides = document.querySelectorAll('.slide')
 const next = document.querySelector('#next')
 const prev = document.querySelector('#prev')
+const getImageBtn = document.querySelector('#get-image-btn')
+const memeModalCloseBtn = document.querySelector('#meme-modal-close-btn')
+const memeModalInner = document.querySelector('#meme-modal-inner')
+const memeModal = document.querySelector('#meme-modal')
+
+getImageBtn.addEventListener('click', renderMeme)
+memeModalCloseBtn.addEventListener('click', closeModal)
 
 function showSlides(index) {
   if (index > slides.length) {slideIndex = 1}
@@ -21,3 +28,19 @@ function minusSlides() {
 showSlides(slideIndex);
 next.addEventListener('click', plusSlides);
 prev.addEventListener('click', minusSlides);
+
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderMeme(){
+    memeModalInner.innerHTML =  `
+        <img 
+        class="meme-modal" 
+        src="./images/blind-driver.jpeg"
+        alt="we silly"
+        >
+        `
+    memeModal.style.display = 'flex'
+}
